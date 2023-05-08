650794b55d95f1baa685c06e12dacbd4ac7a76c3
Sergey <myxxze@gmail.com>
2023-05-08 14:36:22 +0300

    Updated compose & bat.

--- downloadProject.bat
+++ downloadProject.bat
@@ -2,11 +2,10 @@ echo off
 echo Downloading API Gateway...
 git clone https://github.com/srgklmv/ivi-clone-api-gateway
 echo API Gateway downloaded.
-echo Running 'npm i' for API Gateway...
-cd ivi-clone-api-gateway
-npm i
-cd ..
-echo API Gateway packages installed successfully!
+
+echo Downloading Genres MS...
+git clone https://github.com/srgklmv/ivi-clone-genres-ms
+echo Genres MS downloaded.
 
 echo Downloading complete! Now you can start application using docker-compose.
 pause
