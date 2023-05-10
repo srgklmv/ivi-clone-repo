ae8e5ef26d9f0f2669ce9a8860222700c7a6ceeb
Sergey <myxxze@gmail.com>
2023-05-10 19:26:36 +0300

    Fixed bug when script stopped after first 'npm i'.

--- downloadProject.bat
+++ downloadProject.bat
@@ -4,14 +4,14 @@ IF EXIST "ivi-clone-api-gateway" (
     echo Updating Local Repo ivi-clone-api-gateway
     cd ivi-clone-api-gateway
     git pull origin master
-    npm i
+    call npm i
     cd ..
     echo Local Repo has been updated
 ) ELSE (
     echo Downloading API Gateway...
     git clone https://github.com/srgklmv/ivi-clone-api-gateway
     cd ivi-clone-api-gateway
-    npm i
+    call npm i
     cd ..
     echo API Gateway downloaded.
 )
@@ -20,14 +20,14 @@ IF EXIST "ivi-clone-genres-ms" (
     echo Updating Local Repo ivi-clone-genres-ms
     cd ivi-clone-genres-ms
     git pull origin master
-    npm i
+    call npm i
     cd ..
     echo Local Repo has been updated
 ) ELSE (
     echo Downloading Genres MS...
     git clone https://github.com/srgklmv/ivi-clone-genres-ms
     cd ivi-clone-genres-ms
-    npm i
+    call npm i
     cd ..
     echo Genres MS downloaded.
 )
@@ -36,14 +36,14 @@ IF EXIST "ivi-clone-persons-ms" (
     echo Updating Local Repo ivi-clone-persons-ms
     cd ivi-clone-persons-ms
     git pull origin master
-    npm i
+    call npm i
     cd ..
     echo Local Repo has been updated
 ) ELSE (
     echo Downloading Persons MS...
     git clone https://github.com/srgklmv/ivi-clone-persons-ms
     cd ivi-clone-persons-ms
-    npm i
+    call npm i
     cd ..
     echo Persons MS downloaded.
 )
