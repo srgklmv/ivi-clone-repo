20419c2606b7419e3ccc7cfeced4810ed0d798d4
Sergey Klimov <myxxze@gmail.com>
2023-05-10 22:55:26 +0300

    Added all microservices. Should be rewritten to use actual repos.

--- downloadProject.bat
+++ downloadProject.bat
@@ -48,5 +48,85 @@ IF EXIST "ivi-clone-persons-ms" (
     echo Persons MS downloaded.
 )
 
+IF EXIST "ivi-clone-profiles-ms" (
+    echo Updating Local Repo ivi-clone-profiles-ms
+    cd ivi-clone-profiles-ms
+    git pull origin master
+    call npm i
+    cd ..
+    echo Local Repo has been updated
+) ELSE (
+    echo Downloading Profiles MS...
+    git clone https://github.com/srgklmv/ivi-clone-profiles-ms
+    cd ivi-clone-profiles-ms
+    call npm i
+    cd ..
+    echo Profiles MS downloaded.
+)
+
+IF EXIST "ivi-clone-auth-ms" (
+    echo Updating Local Repo ivi-clone-auth-ms
+    cd ivi-clone-auth-ms
+    git pull origin master
+    call npm i
+    cd ..
+    echo Local Repo has been updated
+) ELSE (
+    echo Downloading Auth MS...
+    git clone https://github.com/srgklmv/ivi-clone-auth-ms
+    cd ivi-clone-auth-ms
+    call npm i
+    cd ..
+    echo Auth MS downloaded.
+)
+
+IF EXIST "ivi-clone-comments-ms" (
+    echo Updating Local Repo ivi-clone-comments-ms
+    cd ivi-clone-comments-ms
+    git pull origin master
+    call npm i
+    cd ..
+    echo Local Repo has been updated
+) ELSE (
+    echo Downloading Comments MS...
+    git clone https://github.com/srgklmv/ivi-clone-comments-ms
+    cd ivi-clone-comments-ms
+    call npm i
+    cd ..
+    echo Comments MS downloaded.
+)
+
+IF EXIST "ivi-clone-movies-ms" (
+    echo Updating Local Repo ivi-clone-movies-ms
+    cd ivi-clone-movies-ms
+    git pull origin master
+    call npm i
+    cd ..
+    echo Local Repo has been updated
+) ELSE (
+    echo Downloading Movies MS...
+    git clone https://github.com/srgklmv/ivi-clone-movies-ms
+    cd ivi-clone-movies-ms
+    call npm i
+    cd ..
+    echo Movies MS downloaded.
+)
+
+IF EXIST "ivi-clone-roles-ms" (
+    echo Updating Local Repo ivi-clone-roles-ms
+    cd ivi-clone-roles-ms
+    git pull origin master
+    call npm i
+    cd ..
+    echo Local Repo has been updated
+) ELSE (
+    echo Downloading Roles MS...
+    git clone https://github.com/srgklmv/ivi-clone-roles-ms
+    cd ivi-clone-roles-ms
+    call npm i
+    cd ..
+    echo Roles MS downloaded.
+)
+
 echo Downloading complete! Now you can start application using docker-compose.
 pause
