0ae5d13a3a5990b38954261fdf26b25da1e54823
Sergey Klimov <myxxze@gmail.com>
2023-05-18 05:47:44 +0300

    Rebuilt for current app statement.

--- downloadProject.bat
+++ downloadProject.bat
@@ -19,114 +19,114 @@ IF EXIST "ivi-clone-api-gateway" (
 IF EXIST "ivi-clone-genres-ms" (
     echo Updating Local Repo ivi-clone-genres-ms
     cd ivi-clone-genres-ms
-    git pull origin master
+    git pull origin development
     call npm i
     cd ..
     echo Local Repo has been updated
 ) ELSE (
     echo Downloading Genres MS...
-    git clone https://github.com/srgklmv/ivi-clone-genres-ms
+    git clone -b development https://github.com/srgklmv/ivi-clone-genres-ms
     cd ivi-clone-genres-ms
     call npm i
     cd ..
     echo Genres MS downloaded.
 )
 
-IF EXIST "ivi-clone-persons-ms" (
-    echo Updating Local Repo ivi-clone-persons-ms
-    cd ivi-clone-persons-ms
-    git pull origin master
-    call npm i
-    cd ..
-    echo Local Repo has been updated
-) ELSE (
-    echo Downloading Persons MS...
-    git clone https://github.com/srgklmv/ivi-clone-persons-ms
-    cd ivi-clone-persons-ms
-    call npm i
-    cd ..
-    echo Persons MS downloaded.
-)
+@REM IF EXIST "ivi-clone-persons-ms" (
+@REM     echo Updating Local Repo ivi-clone-persons-ms
+@REM     cd ivi-clone-persons-ms
+@REM     git pull origin master
+@REM     call npm i
+@REM     cd ..
+@REM     echo Local Repo has been updated
+@REM ) ELSE (
+@REM     echo Downloading Persons MS...
+@REM     git clone https://github.com/srgklmv/ivi-clone-persons-ms
+@REM     cd ivi-clone-persons-ms
+@REM     call npm i
+@REM     cd ..
+@REM     echo Persons MS downloaded.
+@REM )
 
-IF EXIST "ivi-clone-profiles-ms" (
-    echo Updating Local Repo ivi-clone-profiles-ms
-    cd ivi-clone-profiles-ms
-    git pull origin master
-    call npm i
-    cd ..
-    echo Local Repo has been updated
-) ELSE (
-    echo Downloading Profiles MS...
-    git clone https://github.com/JcJet/ivi-clone-profiles-ms
-    cd ivi-clone-profiles-ms
-    call npm i
-    cd ..
-    echo Profiles MS downloaded.
-)
+@REM IF EXIST "ivi-clone-profiles-ms" (
+@REM     echo Updating Local Repo ivi-clone-profiles-ms
+@REM     cd ivi-clone-profiles-ms
+@REM     git pull origin master
+@REM     call npm i
+@REM     cd ..
+@REM     echo Local Repo has been updated
+@REM ) ELSE (
+@REM     echo Downloading Profiles MS...
+@REM     git clone https://github.com/JcJet/ivi-clone-profiles-ms
+@REM     cd ivi-clone-profiles-ms
+@REM     call npm i
+@REM     cd ..
+@REM     echo Profiles MS downloaded.
+@REM )
 
-IF EXIST "ivi-clone-auth-ms" (
-    echo Updating Local Repo ivi-clone-auth-ms
-    cd ivi-clone-auth-ms
-    git pull origin master
-    call npm i
-    cd ..
-    echo Local Repo has been updated
-) ELSE (
-    echo Downloading Auth MS...
-    git clone https://github.com/JcJet/ivi-clone-auth-ms
-    cd ivi-clone-auth-ms
-    call npm i
-    cd ..
-    echo Auth MS downloaded.
-)
+@REM IF EXIST "ivi-clone-auth-ms" (
+@REM     echo Updating Local Repo ivi-clone-auth-ms
+@REM     cd ivi-clone-auth-ms
+@REM     git pull origin master
+@REM     call npm i
+@REM     cd ..
+@REM     echo Local Repo has been updated
+@REM ) ELSE (
+@REM     echo Downloading Auth MS...
+@REM     git clone https://github.com/JcJet/ivi-clone-auth-ms
+@REM     cd ivi-clone-auth-ms
+@REM     call npm i
+@REM     cd ..
+@REM     echo Auth MS downloaded.
+@REM )
 
-IF EXIST "ivi-clone-comments-ms" (
-    echo Updating Local Repo ivi-clone-comments-ms
-    cd ivi-clone-comments-ms
-    git pull origin master
-    call npm i
-    cd ..
-    echo Local Repo has been updated
-) ELSE (
-    echo Downloading Comments MS...
-    git clone https://github.com/JcJet/ivi-clone-comments-ms
-    cd ivi-clone-comments-ms
-    call npm i
-    cd ..
-    echo Comments MS downloaded.
-)
+@REM IF EXIST "ivi-clone-comments-ms" (
+@REM     echo Updating Local Repo ivi-clone-comments-ms
+@REM     cd ivi-clone-comments-ms
+@REM     git pull origin master
+@REM     call npm i
+@REM     cd ..
+@REM     echo Local Repo has been updated
+@REM ) ELSE (
+@REM     echo Downloading Comments MS...
+@REM     git clone https://github.com/JcJet/ivi-clone-comments-ms
+@REM     cd ivi-clone-comments-ms
+@REM     call npm i
+@REM     cd ..
+@REM     echo Comments MS downloaded.
+@REM )
 
 IF EXIST "ivi-clone-movies-ms" (
     echo Updating Local Repo ivi-clone-movies-ms
     cd ivi-clone-movies-ms
-    git pull origin master
+    git pull origin development
     call npm i
     cd ..
     echo Local Repo has been updated
 ) ELSE (
     echo Downloading Movies MS...
-    git clone https://github.com/IliaMrt/ivi-clone-movies-ms
+    git clone -b development https://github.com/srgklmv/ivi-clone-movies-ms
     cd ivi-clone-movies-ms
     call npm i
     cd ..
     echo Movies MS downloaded.
 )
 
-IF EXIST "ivi-clone-roles-ms" (
-    echo Updating Local Repo ivi-clone-roles-ms
-    cd ivi-clone-roles-ms
-    git pull origin master
-    call npm i
-    cd ..
-    echo Local Repo has been updated
-) ELSE (
-    echo Downloading Roles MS...
-    git clone https://github.com/JcJet/ivi-clone-roles-ms
-    cd ivi-clone-roles-ms
-    call npm i
-    cd ..
-    echo Roles MS downloaded.
-)
+@REM IF EXIST "ivi-clone-roles-ms" (
+@REM     echo Updating Local Repo ivi-clone-roles-ms
+@REM     cd ivi-clone-roles-ms
+@REM     git pull origin master
+@REM     call npm i
+@REM     cd ..
+@REM     echo Local Repo has been updated
+@REM ) ELSE (
+@REM     echo Downloading Roles MS...
+@REM     git clone https://github.com/JcJet/ivi-clone-roles-ms
+@REM     cd ivi-clone-roles-ms
+@REM     call npm i
+@REM     cd ..
+@REM     echo Roles MS downloaded.
+@REM )
 
 echo Downloading complete! Now you can start application using docker-compose.
 pause
